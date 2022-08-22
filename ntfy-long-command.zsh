490b0513296cbd36f343b7b00d3a1dca16182e81
robfox92 <robfox92@users.noreply.github.com>
2022-08-22 09:51:48 +0800

    better display of durations

--- ntfy-long-command.zsh
+++ ntfy-long-command.zsh
@@ -37,7 +37,11 @@ function ntfy_precmd() {
   if [ $NTFY_TIMER ]; then
     NTFY_DURATION=$(($SECONDS - $NTFY_TIMER))
     if [[ $NTFY_DURATION -gt $NTFY_MIN_SECONDS ]]; then
-      local ntfy_payload="command \""$NTFY_CMD"\" on "$(hostname)" complete (took "$NTFY_DURATION" seconds)"
+      local hours=$(($NTFY_DURATION / 3600))
+      local minutes=$(($(($NTFY_DURATION%3600))/60))
+      local seconds=$(($NTFY_DURATION%60))
+      local display_duration=${hours}:${(l(2)(0))minutes}:${(l(2)(0))seconds}
+      local ntfy_payload="command \""$NTFY_CMD"\" on "$(hostname)" complete (took "$display_duration")"
       curl  -d $ntfy_payload \
             -H "Tags: "$(hostname)"" \
             -S -s -o /dev/null \
